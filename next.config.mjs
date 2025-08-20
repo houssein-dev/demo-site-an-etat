628e415b0a8a8fa63269e19bcf4e862343434e93
houssein-dev <houceinhmd@gmail.com>
2025-08-20 10:37:39 +0100

    Switch to ES modules: update next.config and package.json

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+// const nextConfig = {
+//     output: 'export',
+//     basePath: '/Projet_en_Etat_site',
+//     images: {
+//         unoptimized: true,
+//     },
+// };
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output:"export",
+    assetPrefix: '/demo-site-an-etat/',
+        images: {
+        unoptimized: true,
+    },
+};
+
+export default nextConfig;
